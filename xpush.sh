7408fd5943a9998b982cb769d2dd1799bfc3ff40
Bowen Wan <bowan@microsoft.com>
2020-01-02 12:21:36 +0800

    Remove script.

--- xpush.sh
+++
@@ -1,6 +0,0 @@
-make WHAT=cmd/hyperkube
-export VERSION=v1.13.11-vnet"$1"
-export REGISTRY=bowanacr.azurecr.io
-export HYPERKUBE_BIN=$(pwd)/_output/bin/hyperkube
-make -C cluster/images/hyperkube push
-
